8210cf5f8bbb7f17ba94135053a97c047f44094c
savita-aher <savigahire@gmail.com>
2025-08-01 16:10:37 -0400

    Added part 2

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ for (let num = 1; num <= 100; num++)
         {
         console.log(`Number ${num} is divisible by 5 Print Buzz....`);
         } 
-     
         else  
         {
         console.log(`THis Number is not divisible by 3 & 5 : ${num}`);
@@ -23,3 +22,17 @@ for (let num = 1; num <= 100; num++)
        
     }
 
+//Part 3:
+const str="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz,Master,58\n98,Bill,Doctor's Assistant,26";
+
+let newstring = "";
+for (let i = 0; i < str.length; i++) 
+    {
+    if (str[i] == "\n") {
+        console.log(newstring);
+        newstring = "";
+    } else {
+        newstring += str[i];
+    }
+    }
+ console.log(newstring);
